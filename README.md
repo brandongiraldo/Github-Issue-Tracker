a7f3f91e199cb5beaef53b6d1e9d9a4078fde7d6
brandongiraldo <brandongiraldo10@hotmail.com>
2016-02-25 14:56:31 -0500

    Main styling finished, working on pagination

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ version 0.15.1.
 
 ## Build & development
 
-Install [grunt](http://gruntjs.com/getting-started) and install connect-modrewrite 
+Install [grunt](http://gruntjs.com/getting-started) and install connect-modrewrite with
 ``` npm install connect-modrewrite --save-dev ```
-Run `grunt` for building and `grunt server` to run locally.
+Run `grunt` for building and `grunt server` to run locally on port 9000.
+
+If you wish to configure the port, Gruntfile.js contains server information.
 
 ## Testing
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,28 @@
  * # MainCtrl
  * Controller of the vineIssuetrackerApp
  */
-angular.module('vineIssuetrackerApp')
+angular.module('vineIssuetrackerApp', [])
   .controller('MainCtrl', ['$scope','$http', '$q', '$routeParams', function($scope, $http, $q, $routeParams) {
+  	
   	var defer = $q.defer();
+  	// $scope.filteredTodos = [];
+  	// $scope.currentPage = 1;
+  	// $scope.numPerPage = 9;
+  	// $scope.maxSize = 5;
+
     $http.get("https://api.github.com/repos/npm/npm/issues"). success(function(data) {
     	$scope.data = data;
     	defer.resolve();
     });
+
+    defer.promise.then(function() {
+    	
+    });
+  
+  	// $scope.$watch('currentPage + numPerPage', function() {
+   //  	var begin = (($scope.currentPage - 1) * $scope.numPerPage);
+   //  	var end = begin + $scope.numPerPage;
+   //  	$scope.filteredTodos = $scope.todos.slice(begin, end);
+  	// });
+
   }]);

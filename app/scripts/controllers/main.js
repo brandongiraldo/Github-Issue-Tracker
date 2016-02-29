23ffdef785ecd547dbd2d7c7c63c70f6e4010da3
brandongiraldo <brandongiraldo10@hotmail.com>
2016-02-29 00:20:07 -0500

    Pagination Errors fixed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,32 +25,36 @@ angular.module('vineIssuetrackerApp')
     $scope.pageSize = 9;
     $scope.query = '';
     $scope.data = '';
+    $scope.results = 0; 
 
     $http.get("https://api.github.com/repos/npm/npm/issues").success(function(data) {
     	$scope.data = data;
+      $scope.results = data.length;     
     	defer.resolve();
     });
-    /**
-      numberOfPages will calculated the number of pages remaning given the length
-      of the data and the pagesize (number of elements per page).
-    **/
-    $scope.numberOfPages = function() {
-      return Math.ceil($scope.data.length / $scope.pageSize);
-    };
 
     /**
       After the data is finished procressing, just bind and event listener to setpage to
       1 when searching for an item
     **/
     defer.promise.then(function() {
+      /**
+        numberOfPages will calculated the number of pages remaning given the length
+        of the data and the pagesize (number of elements per page).
+      **/
+      $scope.numberOfPages = function() {
+        return Math.ceil($scope.results / $scope.pageSize);
+      };
+
       $scope.$watch("query", function() {
         if ($scope.query.length > 0) {
-          /**
-            Theres a problem here where the page is back to 1, but
-            that doesnt change the amount of pages of search results.
-            Page 1 of X, where x is static still.
-          **/
           $scope.curPage = 0;
+          if(!$scope.filteredata) {
+            $scope.results = 0;
+          } else {
+            $scope.results = $scope.filteredata.length;
+          }
+          $scope.numberOfPages();
         }
       });
     });
@@ -67,4 +71,33 @@ angular.module('vineIssuetrackerApp').filter('pagination', function(){
     start=+start;
     return input.slice(start);
   };
+});
+
+angular.module('vineIssuetrackerApp').filter('limitToChars', function() {
+  return function(input, limit) {
+    // console.log("Char count - " + input.length + " Input - " + input.split(" "));
+    var currentWordCount = 0;
+    var inputArray = input.split(" ");
+    var outputArray = [];
+    // For all input words
+    for(var i=0; i < inputArray.length; i++) {
+      // If out current count plus another would exceed the limit 
+      if(currentWordCount+inputArray[i].length > limit) {
+        // console.log("Final Char Array - " + outputArray);
+        // console.log("Final Char Count - " + outputArray.join(" ").length);
+        // Return the final outputArray
+        return outputArray.join(" ");
+      } else {
+        // If we reached the end of the array
+        if(i == inputArray.length-1) {
+          return outputArray.join(" ");
+          // Otherwise lets keep counting and storing
+        } else {
+          outputArray.push(inputArray[i]); 
+          // console.log("Word Array so far - " + outputArray);
+          currentWordCount+=inputArray[i].length+1; // +1 for space
+        }
+      }
+    }
+  }
 });
